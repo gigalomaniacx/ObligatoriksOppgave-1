0998868f06949dc39e8b4ac93503261be287adb8
Adrian <adgrjb@gmail.com>
2021-06-02 13:35:40 +0200

    empety files for now that I'll populate eventually

---
+++ model.js
@@ -0,0 +1,3 @@
+const app = document.getElementById('app');
+let earthWeight = 69;
+let planet = 'Mercury';
---
+++ view.js
@@ -0,0 +1,9 @@
+updateView();
+function updateView() {
+    app.innerHTML = `
+        <h1>Tjohei da var vi igang!</h1>
+        <input type="text" value="${earthWeight}"/>
+        <select id="planets" 
+        <button>calculate weight on ${planet}</onclick>
+    `;
+}
