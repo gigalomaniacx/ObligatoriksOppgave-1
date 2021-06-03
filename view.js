0c5b6c1beae44e884f07613e3ddd3e3949f8b8e5
Adrian <adgrjb@gmail.com>
2021-06-03 12:28:25 +0200

    Put all the view code into view

--- view.js
+++ view.js
@@ -1,9 +1,21 @@
 updateView();
 function updateView() {
-    app.innerHTML = `
-        <h1>Tjohei da var vi igang!</h1>
-        <input type="text" value="${earthWeight}"/>
-        <select id="planets" 
-        <button>calculate weight on ${planet}</onclick>
+    document.getElementById('app').innerHTML = `
+        <div id="boks" class="float-div" onmouseover="setBoxShadow()">
+            <h1>Tjohei da var vi igang!</h1>
+            <input id="earthWeightInput" type="text" value="${earthWeight}"/>
+            <select id="planets">
+                <option value="Mercury">Mercury</option>
+                <option value="Venus">Venus</option>
+                <option value="Moon">Moon</option>
+                <option value="Mars">Mars</option>
+                <option value="Jupiter">Jupiter</option>
+                <option value="Saturn">Saturn</option>
+                <option value="Uranus">Uranus</option>
+            </select>
+            <button onclick="calculatePlanetWeight()">calculate weight on ${planet}</button>
+                ${returnPlanetaryWeight}
+        </div>
     `;
+    
 }
