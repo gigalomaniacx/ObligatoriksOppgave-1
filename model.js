633ddbc49b393fcd165af1836133bcc026e2643c
Adrian <adgrjb@gmail.com>
2021-06-03 14:28:46 +0200

    Did the earthWeight id call in the declaration

--- model.js
+++ model.js
@@ -1,6 +1,6 @@
 // const app = document.getElementById('app');
 let planetDropDown;
-let earthWeight;
+let earthWeight = document.getElementById('earthWeightInput');
 let planet;
 let planetWeight;
 let returnPlanetaryWeight = '';
