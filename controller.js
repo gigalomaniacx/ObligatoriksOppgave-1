3fa9a304c499779d622dc3549b5de6f624446344
Adrian <adgrjb@gmail.com>
2021-06-03 14:30:14 +0200

    Can't get the logic to clear the textfield if null

--- controller.js
+++ controller.js
@@ -1,11 +1,15 @@
 function setEarthWeightBlank() {
     returnPlanetaryWeight = `Please enter a weight in the txt field`;
-    document.getElementById('earthWeightInput').value = '';
+    earthWeight = '';
 }
 
 function returnMessage(){
-    document.getElementById('earthWeightInput').value == null || document.getElementById('earthWeightInput').value == undefined ? setEarthWeightBlank():
-    returnPlanetaryWeight = `Your weight on the planet: ${planet} is ${planetWeight}.`;
+    if (earthWeight != null && earthWeight != '') {
+        returnPlanetaryWeight = `Your weight on the planet: ${planet} is ${planetWeight}.`
+    }
+    else{
+        setEarthWeightBlank();
+    }
 }
 
 function calculatePlanetWeight() {
@@ -20,60 +24,43 @@ function calculatePlanetWeight() {
     planetDropDown.value == 'Saturn' ? getPlanetWeight('Saturn', 1.065) :
     planetDropDown.value == 'Uranus' ? getPlanetWeight('Uranus', 0.886) :
     'Error that is not handled D: (ups)';
-//     if (planetDropDown.value == 'Mercury'){
-//         // planetWeight = earthWeight * 0.38;
-//         // planetWeight = getPlanetWeight(0.38);
-        
-//         getPlanetWeight('Mercury',0.38);
-//         // returnPlanetaryWeight = `Your weight on the planet: ${planet} is ${planetWeight}.`;
-//         // returnMessage();
-//         // updateView();
-//     }
-//     if (planetDropDown.value == 'Venus') {
-        
-//         getPlanetWeight('Venus',0.904);
-//     }
-//     if (planetDropDown.value == 'Moon') {
-        
-//         getPlanetWeight('Moon', 0.1654);
-//     }
-//     if (planetDropDown.value == 'Mars') {
-        
-//         getPlanetWeight('Mars', 0.1654);
-//     }
-//     if (planetDropDown.value == 'Jupiter') {
-        
-//         getPlanetWeight('Jupiter', 2.528);
-//     }
-//     if (planetDropDown.value == 'Saturn') {
-        
-//         getPlanetWeight('Saturn', 1.065);
-//     }
-//     if (planetDropDown.value == 'Uranus') {
-        
-//         getPlanetWeight('Uranus', 0.886);
-//     }
 }
 
- //planet weight calculating & halfprint
- function getPlanetWeight(planetName,Gs){
+//function that returns a value with return(exercise requirement)
+//used to calculate the planet weight.
+function gravityCalc(tall){
+    let sum = earthWeight * tall;
+    return sum;
+}
+
+//planet weight calculating & halfprint
+function getPlanetWeight(planetName,Gs){
     planet = planetName;
-    planetWeight = earthWeight * Gs;
+    planetWeight = gravityCalc(Gs);
+    // planetWeight = earthWeight * Gs;
     planetDropDown.value = planetName;
     returnMessage();
     updateView();
 }
 
-//background swag
-function setBoxShadow(){
-    let boxDiv = document.body;//getElementById('boks');
+//helper function for nested if statements to apply css class
+function setCssClass(addClass) {
+    let classAdd = addClass;
+    classDel = document.body.classList.item(0);
+    document.body.classList.remove(classDel);
+    document.body.classList.add(classAdd);
+}
+
+//background image class logic
+function bodyBackgroundImg(){
+    
     planet == null || planet == undefined ? planet = 'Mercury' :
-    planet == 'Mercury' ? boxDiv.classList.add('mercury-div') :
-    planet == 'Venus' ? boxDiv.classList.add('venus-div') :
-    planet == 'Moon' ? boxDiv.classList.add('moon-div') :
-    planet == 'Mars' ? boxDiv.classList.add('mars-div') :
-    planet == 'Jupiter' ? boxDiv.classList.add('jupiter-div') :
-    planet == 'Saturn' ? boxDiv.classList.add('saturn-div') :
-    planet == 'Uranus' ? boxDiv.classList.add('uranus-div') :
+    planet == 'Mercury' ? setCssClass('mercury-div') :
+    planet == 'Venus' ? setCssClass('venus-div') :
+    planet == 'Moon' ? setCssClass('moon-div') :
+    planet == 'Mars' ? setCssClass('mars-div') :
+    planet == 'Jupiter' ? setCssClass('jupiter-div') :
+    planet == 'Saturn' ? setCssClass('saturn-div') :
+    planet == 'Uranus' ? setCssClass('uranus-div') :
     console.log('error boxShadow');
 }
